ca4e54acfdd3bec4570880f0f19fbc08e2ffdb6a
Luke <qscrt758459@gamil.com>
2024-12-05 14:21:12 +0800

    修改readme.php 裡面的解釋

--- readme.php
+++ readme.php
@@ -1,6 +1,10 @@
 <?php
 
 echo "這是一個寶石販賣的電商，使用 MAMP 以及 內建的 MYSQL 資料庫
-      使用localhost:8888 即可使用網站測試";
+      1.使用 mac book 開啟 MAMP 連線後
+      2.打開Website , 點擊左上角website
+      3.即可看到網頁呈現
+      4.或者使用localhost:8888 即可使用網站測試
+      5.職員登入帳號:123@gmail.com 密碼:123";
 
 
